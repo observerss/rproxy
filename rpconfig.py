5c33342e4d386e1029da98708a6091b3373d7a29
observerss <root@vps19.vz27.ramhost.us>
2010-11-12 01:27:10 +0000

    add files

---
+++ rpconfig.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+import re
+from hashlib import md5
+
+class RPConfig:
+    def __init__(self,path,domain="reverse-proxy.org"):
+        self.path = path
+        self.domain = domain
+        self.cfgs = []
+        self.cfgdict = {}
+        self.externs = []
+        self.read_config()
+
+    def add_alias(self,target,alias,html,javascript,css,flash,extern):
+        key = alias+"."+self.domain
+        value = (target,alias,html,javascript,css,flash,extern)
+        self.cfgdict[key]=value
+        cfgs = []
+        for x in self.cfgs:
+            if x[1] != alias:
+                cfgs.append(x)
+        cfgs.append( value )
+        self.cfgs = cfgs
+        if (extern == "Y") and (key not in self.externs):
+            self.externs.append(key)
+        self.save()
+
+    def del_alias(self,alias):
+        key = alias+"."+self.domain
+        if self.cfgdict.has_key(key):
+            self.cfgdict.pop(key)
+        cfgs = []
+        for x in self.cfgs:
+            if x[1] != alias:
+                cfgs.append(x)
+        self.cfgs = cfgs
+        if key in self.externs:
+            self.externs.remove(key)
+        self.save()
+
+    def get_config(self,alias):
+        key = alias+"."+self.domain
+        return self.cfgdict[key]
+
+    def striphost(self,host):
+        return '.'.join( host.split('.')[-3:] )
+
+    def get_realhost(self,host):
+        shost = self.striphost(host)
+        port = 80
+        if self.cfgdict.has_key(shost):
+            target = self.cfgdict[shost][0]
+            if ':' in target:
+                port = int(target[target.find[':']+1:])
+            realhost = host.replace(shost,target)
+            return realhost,port
+        return host,port
+
+    def get_aliasheader(self,host,value):
+        shost = self.striphost(host)
+        if self.cfgdict.has_key(shost):
+            target = self.cfgdict[shost][0]
+            return value.replace(target,shost)
+        return value
+
+    def get_realheader(self,host,value):
+        shost = self.striphost(host)
+        if self.cfgdict.has_key(shost):
+            return value.replace(shost,self.cfgdict[shost][0])
+        return value
+
+    def process(self,host,ctype,data):
+        '''replace url if necaessary'''
+        host = self.striphost(host)
+        # replace global settings
+        for ext in self.externs:
+            if host!=ext:
+                data = self._process(ext,ctype,data)
+        # replace local settings
+        data = self._process(host,ctype,data)
+        # some spefic repl for facebook,twitter,etc
+        data = self.__process(host,ctype,data)
+        return data
+
+    def _process(self,host,ctype,data):
+        if self.cfgdict.has_key(host):
+            target,alias,html,css,js,flash,extern = self.cfgdict[host]
+            if (ctype.startswith("text/html") and html == "Y") or \
+                    (ctype.startswith("text/css") and css == "Y") or \
+                    ("javascript" in ctype and js == "Y") or \
+                    ("text" in ctype):
+                    #(ctype.startswith("application/x-shockwave-flash") and flash == "Y"):
+                data = data.replace(target,host)
+        return data
+
+    def __process(self,host,ctype,data):
+        if self.cfgdict.has_key(host):
+            target,alias,html,css,js,flash,extern = self.cfgdict[host]
+            if target == "facebook.com" and "javascript" in ctype:
+                data = data.replace(r"'\/\/www.'+e+'.com\/ajax\/ua_callback.php'",
+                    r"'\/\/%s\/ajax\/ua_callback.php'"%host )
+            if target == "twitter.com":
+                data = data.replace("document.domain = 'twitter.com';",  r"")
+            if target == "twimg.com" and "javascript" in ctype:
+                data = re.sub(r"document\.domain\s*=\s*('|\")twitter\.com('|\")\s*;?", r"", data)
+        return data
+
+    def save(self):
+        self.write_config()
+
+    def read_config(self):
+        cfgs = open(self.path).readlines()
+        for cfg in cfgs:
+            cfg = self.format_config(cfg)
+            if cfg:
+                try:
+                    target,alias,html,css,js,flash,extern = cfg.split()
+                    args = self.format_check(target,alias,html,css,js,flash,extern)
+                    self.cfgs.append( args )
+                    self.cfgdict[alias+"."+self.domain] = args
+                    if extern == "Y":
+                        self.externs.append( alias+"."+self.domain )
+                except:
+                    pass
+
+    def write_config(self):
+        cfgpath = open(self.path,"w")
+        docs = '''# This is the config file for rproxy
+# Each line is a rproxy entry, represents target,alias,html,css,js,flash,global
+# the format is as follows:
+# TARGET ALIAS Y/N Y/N Y/N Y/N Y/N
+# e.g: 
+# www.google.com gg Y Y Y Y Y
+# obmem.com AUTO Y Y Y Y Y
+'''
+        cfgpath.write(docs)
+        for cfg in self.cfgs:
+            cfgpath.write("%s %s %s %s %s %s %s\n"%cfg)
+        
+    def format_config(self,line):
+        #remove comment
+        line = re.sub(r'#.*','',line)
+        #strip
+        line = line.strip()
+        return line
+
+    def format_check(self,target,alias,html,css,js,flash,extern):
+        html = html.upper() in ["Y","N"] and html.upper() or "N"
+        css = css.upper() in ["Y","N"] and css.upper() or "N"
+        js = js.upper() in ["Y","N"] and js.upper() or "N"
+        flash = flash.upper() in ["Y","N"] and flash.upper() or "N"
+        extern = extern.upper() in ["Y","N"] and extern.upper() or "N"
+        return (target,alias,html,css,js,flash,extern)
+
+if __name__ == "__main__":
+    rpconf = RPConfig("rproxy.cfg")
+    rpconf.add_alias("playsc.com","playsc","Y","Y","Y","Y","Y")
+    rpconf.del_alias("playsc")
---
+++ rproxy.py
@@ -0,0 +1,219 @@
+from twisted.internet import epollreactor
+epollreactor.install()
+
+from twisted.internet import protocol,reactor,ssl
+from twisted.protocols.basic import LineReceiver
+
+from twisted.application.service import Application
+from twisted.application import internet,app
+
+from twisted.web import http,server
+from twisted.web.proxy import Resource,ProxyClientFactory,ProxyClient,ReverseProxyResource
+from twisted.web.server import NOT_DONE_YET
+from pickle import load,dump
+import StringIO,gzip,zlib
+import os,sys
+from mako.template import Template
+
+PATH = "/var/www/rproxy"
+sys.path.append(PATH)
+from rpconfig import RPConfig
+
+DEBUG = False
+CONFPATH = PATH + "/rproxy.cfg"
+DOMAIN = "rproxy.org"
+PORT = 8484
+
+class MyProxyClient(ProxyClient):
+    def __init__(self, *args, **kwargs):
+        self._buffer = ''
+        self.encoding = ''
+        self.ctype = ''
+        self.reencode = True
+        self.replace = False
+        ProxyClient.__init__(self,*args,**kwargs)
+
+    def handleHeader(self, key, value):
+        value = self.factory.rp.get_aliasheader(self.factory.host,value) 
+        if key == 'Cache-Control':
+            value = 'public'
+        if DEBUG:
+            print key,value
+        if key == "Content-Type" and (value.startswith("text") or \
+                ("java" in value)):
+            self.replace = True
+            self.ctype = value
+        if key == "Content-Encoding":
+            self.encoding = value
+            return
+        if key == "Content-Length":
+            return
+        else:
+            ProxyClient.handleHeader(self, key, value)
+ 
+    def handleEndHeaders(self):
+        #let handleResponseEnd do this
+        pass 
+
+    def handleResponsePart(self, buffer):
+        if self.replace:
+            self._buffer += buffer
+        else:
+            ProxyClient.handleResponsePart(self,buffer)
+
+    def handleResponseEnd(self):
+        if self.replace:
+            if self.encoding == 'gzip':
+                try:
+                    buffer1 = StringIO.StringIO(self._buffer)
+                    gzipper = gzip.GzipFile(fileobj=buffer1)
+                    html = gzipper.read()
+                except Exception as what:
+                    print self.factory.realhost,what
+                    html = self._buffer
+            elif self.encoding == 'deflate':
+                try:
+                    html = zlib.decompress(self._buffer)
+                except zlib.error:
+                    html = zlib.decompress(self._buffer, -zlib.MAX_WBITS)
+            else:
+                html = self._buffer
+            self._buffer = self.factory.rp.process(self.factory.host,self.ctype,html)
+            if self.reencode:
+                newbuffer = StringIO.StringIO()
+                gzipper = gzip.GzipFile(fileobj=newbuffer,mode='wb')
+                gzipper.write(self._buffer)
+                gzipper.close()
+                self._buffer = newbuffer.getvalue()
+                ProxyClient.handleHeader(self,"Content-Encoding","gzip")
+            ProxyClient.handleHeader(self, "Content-Length", len(self._buffer))
+            ProxyClient.handleEndHeaders(self)
+            ProxyClient.handleResponsePart(self,self._buffer)
+        else:
+            if self.encoding:
+                ProxyClient.handleHeader(self,"Content-Encoding",self.encoding)
+            ProxyClient.handleEndHeaders(self)
+        ProxyClient.handleResponseEnd(self) 
+
+class MyProxyClientFactory(ProxyClientFactory):
+    protocol = MyProxyClient
+    def __init__(self, *args, **kwargs):
+        self.host = ''
+        self.realhost = ''
+        self.rp = None
+        ProxyClientFactory.__init__(self,*args,**kwargs)
+
+    def buildProtocol(self, addr):
+        p = self.protocol(self.command, self.rest, self.version,
+                             self.headers, self.data, self.father)
+        p.factory = self
+        return p
+
+
+class MyReverseProxyResource(Resource): 
+    proxyClientFactoryClass = MyProxyClientFactory
+    def __init__(self,reactor=reactor):
+        Resource.__init__(self)
+        self.rp = RPConfig(CONFPATH,domain=DOMAIN)
+        self.reactor = reactor
+        self.host = ''
+        self.port = 80
+        self.realhost = ''
+
+    def getChild(self, path, request):
+        """
+        Create and return a proxy resource with the same proxy configuration
+        as this one, except that its path also contains the segment given by
+        C{path} at the end.
+        """
+        return MyReverseProxyResource(self.reactor)
+    
+    def render(self, request):
+        """
+        Render a request by forwarding it to the proxied server.
+        """
+        self.host = request.received_headers['host']
+        if self.host == DOMAIN or \
+            self.host == "www."+DOMAIN:
+            return self.confpage(request)
+        
+        self.realhost,self.port = self.rp.get_realhost(self.host)
+        # if not redirect is found, confpage again
+        if self.realhost == self.host:
+            return self.confpage(request)
+
+        if request.getHeader("x-forwarded-proto")=="https":
+            self.port = 443
+
+        for k,v in request.getAllHeaders().items():
+            request.received_headers[k] = self.rp.get_realheader(self.host,v)
+#        value = request.getHeader("referer")
+#        if value:
+#            request.received_headers['referer']=self.rp.get_realheader(self.host,value)
+        request.received_headers['host'] = self.realhost
+        request.content.seek(0, 0)
+        clientFactory = self.proxyClientFactoryClass(
+            request.method, request.uri, request.clientproto,
+            request.getAllHeaders(), request.content.read(), request)
+        if DEBUG:
+            print self.realhost,self.port
+            print request.method
+            print request.uri
+            print request.clientproto
+            print request.getAllHeaders()
+            print request.content.read()
+        clientFactory.host = self.host
+        clientFactory.realhost = self.realhost
+        clientFactory.rp = self.rp
+        if self.port == 443:
+            self.reactor.connectSSL(self.realhost,self.port,clientFactory,ssl.ClientContextFactory())
+        else:
+            self.reactor.connectTCP(self.realhost,self.port,clientFactory)
+        return NOT_DONE_YET
+
+    def page_index(self):
+            html = Template(open("index.html").read()).render(cfgs=self.rp.cfgs,domain=DOMAIN)
+            return html
+
+    def confpage(self,request):
+        if request.uri == "/":
+            return self.page_index()
+        elif request.uri.startswith("/del"):
+            alias = request.uri.split("=")[-1]
+            self.rp.del_alias(alias)
+            return self.page_index()
+        elif request.uri.startswith("/add"):
+            cfg = ['','','','','','','']
+            html = Template(open("edit.html").read()).render(cfg=cfg)
+            return html
+        elif request.uri.startswith("/edit"):
+            if request.method == "GET":
+                alias = request.uri.split("=")[-1]
+                cfg = self.rp.get_config(alias)
+                html = Template(open("edit.html").read()).render(cfg=cfg)
+                return html
+            elif request.method == "POST":
+                # get post param
+                # self.rp.add_alias()
+                return self.page_index() 
+        else:
+            if os.path.exists(request.uri[1:]):
+                if request.uri[1:].endswith('css'):
+                    request.setHeader('content-type', 'text/css')
+                elif request.uri[1:].endswith('jpg'):
+                    request.setHeader('content-type', 'image/jpeg')
+                else:
+                    request.setHeader('content-type', 'text/plain')
+                return open(request.uri[1:]).read()
+            else:
+                return "Not Found"
+ 
+site = server.Site(MyReverseProxyResource())
+#site = server.Site(ReverseProxyResource('www.google.com',80,''))
+reactor.listenTCP(PORT, site)
+reactor.run()
+application = Application("RProxy")
+appService = internet.TCPServer(PORT,site)
+appService.setServiceParent(application)
+
+
